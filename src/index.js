d1e1d3137082a8d64c4f7b9a8d89beefe2bc7a3f
47seconds <47seconds@NeXa.localdomain>
2024-07-06 01:13:23 +0530

    added a log to specify server url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { app } from './app.js';
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 3000, () => {
-        console.log(`sever running on http://localhost:${process.env.PORT}...`);
+        console.log(`sever running on http://localhost:${process.env.PORT}/api/v1...`);
     });
     app.on('errror', (error) => {
         console.log("ERRROR: ", error);
