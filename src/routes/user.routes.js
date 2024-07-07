48c73302b6269d0d3ccd46892ffeca96538007e2
47seconds <47seconds@NeXa.localdomain>
2024-07-07 17:31:40 +0530

    updated user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { userRegistration } from "../controllers/user.controller.js";
+import {upload} from '../middlewares/multer.middleware.js';
 
 const userRouter = Router();
 
-userRouter.route('/register').post(userRegistration);
-userRouter.route('/register').get(userRegistration);
+userRouter.route('/register').post(
+    upload.fields([         // Added file upload middleware for avatar, and coverImage from frontend when user registers
+        {
+            name: 'avatar',
+            maxCount: 1
+        },
+        {
+            name: 'coverImage',
+            maxCount: 1
+        }
+    ]),
+    userRegistration
+);
 
 export default userRouter;  
