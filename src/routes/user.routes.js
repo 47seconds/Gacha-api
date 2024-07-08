0448fc1552471d37c9c469eb014cfdb63644e46a
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-09 02:33:17 +0530

    added delete user route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,28 +1,37 @@
 import { Router } from "express";
-import { userLogin, userLogout, userRegistration, userRefreshToken } from "../controllers/user.controller.js";
-import {upload} from '../middlewares/multer.middleware.js';
-import {verifyJWT} from '../middlewares/auth.middleware.js';
+import {
+    userLogin,
+    userLogout,
+    userRegistration,
+    userRefreshToken,
+    userDelete,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.route('/register').post(
-    upload.fields([         // Added file upload middleware for avatar, and coverImage from frontend when user registers
+userRouter.route("/register").post(
+    upload.fields([
+        // Added file upload middleware for avatar, and coverImage from frontend when user registers
         {
-            name: 'avatar',
-            maxCount: 1
+            name: "avatar",
+            maxCount: 1,
         },
         {
-            name: 'coverImage',
-            maxCount: 1
-        }
+            name: "coverImage",
+            maxCount: 1,
+        },
     ]),
     userRegistration
 );
 
-userRouter.route('/login').post(userLogin);
+userRouter.route("/login").post(userLogin);
 
-userRouter.route('/logout').post(verifyJWT, userLogout);    // authenticate user before logout using access token
+userRouter.route("/logout").post(verifyJWT, userLogout); // authenticate user before logout using access token
 
-userRouter.route('/refresh-token').post(userRefreshToken);   // no need to authentiate, we are generating new access token, so verifuJWT method wont work
+userRouter.route("/refresh-token").post(userRefreshToken); // no need to authentiate, we are generating new access token, so verifuJWT method wont work
 
-export default userRouter;      
+userRouter.route("/delete-account").delete(verifyJWT, userDelete);        // delete account, but we need req.url first
+
+export default userRouter;
