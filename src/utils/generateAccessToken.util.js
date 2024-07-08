b1f83ea9427e8854c655872a3fb7f56d81461aec
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-08 23:02:16 +0530

    added method for generating access token

---
+++ src/utils/generateAccessToken.util.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.util.js";
+import { asyncHandler } from "../utils/asyncHandler.util.js";
+import jwt from 'jsonwebtoken';
+import { generateAccessAndRefreshToken } from "./generateAccessAndRefreshToken.util.js";
+
+export const generateAccessToken = asyncHandler(async (req, res) => {
+    // Look for refresh token from cookies
+    const cookieRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+    try {
+        const decodedRefreshToken = jwt.verify(cookieRefreshToken, REFRESH_TOKEN_SECRET);
+        if(!decodedRefreshToken) throw new ApiError(401, "unauthorized request");
+    
+        // Look for user from decoded token's payload
+        const requestUser = await User.findById(decodedRefreshToken?._id);
+        if(!requestUser) throw new ApiError(400, "Invalid refresh token");
+    
+        // Compare refresh tokens from decoded and from database
+        if(requestUser?.refreshToken !== decodedRefreshToken) throw new ApiError(401, "refresh token expired or invalid");
+    } catch (error) {
+        throw new ApiError(500, "failed to verify refresh token");
+    }
+
+    // Refresh refresh token and give new access token
+    const {accessToken, refreshToken, user} = generateAccessAndRefreshToken(requestUser._id);
+    return {accessToken, refreshToken, user};
+});
