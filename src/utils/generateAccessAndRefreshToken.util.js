2a447a59c636a0b769a7ed09cfe092ce9a559a33
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-08 23:04:33 +0530

    fixed document forrmat with prettier

--- src/utils/generateAccessAndRefreshToken.util.js
+++ src/utils/generateAccessAndRefreshToken.util.js
@@ -2,16 +2,17 @@ import { ApiError } from "./ApiError.util.js";
 import { User } from "../models/user.model.js";
 
 export const generateAccessAndRefreshToken = async (userId) => {
-    try{
-        const user = await User.findById(userId);
-        const accessToken = await user.generateAccessToken();
-        const refreshToken = await user.generateRefreshToken();
+  try {
+    const user = await User.findById(userId);
 
-        user.refreshToken = refreshToken;
-        await user.save({ValidieBeforeSave: false});
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
 
-        return {accessToken, refreshToken, user}
-    } catch {
-        throw new ApiError(500, "failed to generate access and refresh token. Please try again");
-    }
-}
+    user.refreshToken = refreshToken;
+    await user.save({ ValidieBeforeSave: false });
+
+    return { accessToken, refreshToken, user };
+  } catch {
+    throw new ApiError(500, "failed to generate access and refresh token");
+  }
+};
