fb46fd6b9da2eeb3da658d894169c625e08e5caf
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-10 00:53:09 +0530

    added user check

--- src/utils/generateAccessAndRefreshToken.util.js
+++ src/utils/generateAccessAndRefreshToken.util.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 export const generateAccessAndRefreshToken = async (userId) => {
   try {
     const user = await User.findById(userId);
+    if(!user) throw new ApiError(400, "no such user");
 
     const accessToken = await user.generateAccessToken();
     const refreshToken = await user.generateRefreshToken();
