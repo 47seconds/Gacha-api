392305553eb20319fa5640b34127a27856a06370
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-21 20:10:39 +0530

    added mongoose aggregate paginate module for aggregation pipelining

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
 import mongoose, {Schema} from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
         const userSchema = new Schema(
             {
